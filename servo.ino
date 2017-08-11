8e48716ebec5d4acf262be9a6f79ae3343014a9d
staryes <staryes@users.noreply.github.com>
2017-08-11 12:39:16 -0400

    Update and rename jul22a.ino to servo.ino

--- servo.ino
+++ servo.ino
@@ -4,7 +4,7 @@ Servo servo1;
 void setup() {
   // put your setup code here, to run once:
   pinMode(1,OUTPUT);
-  servo1.attach(12,1600,1900); //analog pin 0 
+  servo1.attach(12,1500,1900); //analog pin 0 
   //servo1.setMaximumPulse(1900);
   //servo1.setMinimumPulse(1500);
 
@@ -16,7 +16,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   static int v = 0;
-  int deg = 90;
+  static int deg = 90;
   if ( Serial.available()) 
   {
     char ch = Serial.read();
@@ -29,19 +29,20 @@ void loop() {
           deg = 90;
         break;
       case 'd':
-          deg = 45;
+          deg = 120;
         break;
       case 's':
           deg = v;
+          v = 0;
           break;
       default:
         break;
     }    
     
   }
-    servo1.write(130);
+    servo1.write(deg);
     delay(500);
-    //servo1.write(80);
+    //servo1.write(90);
     //delay(500);
 }
 
